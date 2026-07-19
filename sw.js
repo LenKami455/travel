06f9c5abf363db8f7413363f5e3049608ef3205a
Len Kisaragi <lenkaren0905@example.com>
2026-07-19 10:37:17 +0900

    feat: ブラウザタブ用favicon追加(アンバー月+水平線の簡略版)
    Nagi.pngは文字入りの線画で小サイズだと潰れるため、favicon専用に
    月+水平線だけを太く再描画。sw.jsのCACHEをv6.1.1にバンプ。

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
 /* ロケハンダッシュボード service worker */
-const CACHE = "nagi-v6.1.0";
+const CACHE = "nagi-v6.1.1";
 const ASSETS = [
   "./", "./index.html", "./manifest.webmanifest",
-  "./icons/Nagi.png"
+  "./icons/Nagi.png",
+  "./favicon.ico",
+  "./icons/favicon-16.png", "./icons/favicon-32.png", "./icons/favicon-48.png"
 ];
 
 self.addEventListener("install", e => {
